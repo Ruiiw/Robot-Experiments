16297023e65a273221b0890d012c689518830926
Rui Wen <ruiwen@dhcp-10-105-215-29.wireless.northwestern.private>
2023-01-08 17:42:32 -0600

    finished two-link one-joint robot part

--- generate.py
+++ generate.py
@@ -1,32 +1,26 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("boxes.sdf")
-length = 1
-width = 1
-height = 1
 
-x = 0
-y = 0
-z = 0.5
-
-x2 = 1
-y2 = 0
-z2 = 1.5
-# pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
-# pyrosim.Send_Cube(name="Box2", pos=[x2, y2, z2] , size=[length, width, height])
-
-x3 = 0
-y3 = 0
-for i in range(8):
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
     length = 1
     width = 1
     height = 1
-    for j in range(8):
-        length = 1
-        width = 1
-        height = 1
-        for k in range(8):
-            pyrosim.Send_Cube(name="Box", pos=[x3 + j, y3 + i, z + k] , size=[length, width, height])
-            length *= 0.9
-            width *= 0.9
-            height *= 0.9
-pyrosim.End()
+    x = -2
+    y = 2
+    z = 0
+    pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
+    pyrosim.End()
+
+Create_World()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    length = 1
+    width = 1
+    height = 1
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0] , size=[length, width, height])
+    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5, 0, 1])
+    pyrosim.Send_Cube(name="Leg", pos=[1, 0, 1.5] , size=[length, width, height])
+    pyrosim.End()
+
+Create_Robot()
