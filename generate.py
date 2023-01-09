f2e7776f2d8321469877501878c735271e804111
Rui Wen <ruiwen@dhcp-10-105-215-29.wireless.northwestern.private>
2023-01-09 14:52:03 -0600

    finished robot

--- generate.py
+++ generate.py
@@ -33,19 +33,19 @@ def Create_Robot():
     pyrosim.Send_Cube(name="Link6", pos=[0, 0, -0.5] , size=[length, width, height])
     pyrosim.End()
 
-Create_Robot()
+# Create_Robot()
 
 
-# def Create_3Link_2Joint_Robot():
-#     pyrosim.Start_URDF("body.urdf")
-#     length = 1
-#     width = 1
-#     height = 1
-#     pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1.5] , size=[length, width, height])
-#     pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0, -0.5, 0])
-#     pyrosim.Send_Cube(name="BackLeg", pos=[0, -0.5, 0.5] , size=[length, width, height])
-#     pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0, 0.5, 0])
-#     pyrosim.Send_Cube(name="FrontLeg", pos=[0, 0.5, 0.5] , size=[length, width, height])
-#     pyrosim.End()
+def Create_3Link_2Joint_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    length = 1
+    width = 1
+    height = 1
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5] , size=[length, width, height])
+    pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1, 0, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5] , size=[length, width, height])
+    pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2, 0, 1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5] , size=[length, width, height])
+    pyrosim.End()
 
-# Create_3Link_2Joint_Robot()
+Create_3Link_2Joint_Robot()
