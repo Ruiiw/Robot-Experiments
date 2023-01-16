f3d1de05f1adef8ce8559f7ca41677586ec27470
Rui Wen <ruiwen@dhcp-10-105-215-29.wireless.northwestern.private>
2023-01-16 00:04:48 -0600

    generated sin movement graph

--- analyse.py
+++ analyse.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot
 
 frontLegSensorValues = numpy.load('data/frontLegSensorData.npy')
 backLegSensorValues = numpy.load('data/backLegSensorData.npy')
+targetAngles = numpy.load('data/targetAngles.npy')
 print(backLegSensorValues)
-matplotlib.pyplot.plot(frontLegSensorValues, linewidth = 3, label = 'front leg')
-matplotlib.pyplot.plot(backLegSensorValues, label = 'back leg')
+# matplotlib.pyplot.plot(frontLegSensorValues, linewidth = 3, label = 'front leg')
+# matplotlib.pyplot.plot(backLegSensorValues, label = 'back leg')
+matplotlib.pyplot.plot(targetAngles)
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import pybullet_data
 import time
 import pyrosim.pyrosim as pyrosim
 import numpy
+import random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -15,14 +16,30 @@ p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 frontLegSensorValues = numpy.zeros(1000)
 backLegSensorValues = numpy.zeros(1000)
+linAngles = numpy.linspace(0, 2*numpy.pi, 1000)
+targetAngles = numpy.sin(linAngles)
+numpy.save('data/targetAngles.npy', targetAngles)
+
 for i in range(1000):
     p.stepSimulation()
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex = robotId,
+    jointName = "Torso_BackLeg",
+    controlMode = p.POSITION_CONTROL,
+    targetPosition = random.uniform(-numpy.pi/4, numpy.pi/4),
+    maxForce = 60)
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex = robotId,
+    jointName = "Torso_FrontLeg",
+    controlMode = p.POSITION_CONTROL,
+    targetPosition = random.uniform(-numpy.pi/4, numpy.pi/4),
+    maxForce = 60)
     time.sleep(1/60)
 
-print(frontLegSensorValues)
-print(backLegSensorValues)
+# print(frontLegSensorValues)
+# print(backLegSensorValues)
 numpy.save('data/frontLegSensorData.npy', frontLegSensorValues)
 numpy.save('data/backLegSensorData.npy', backLegSensorValues)
 p.disconnect()
