a0cc3bf78f1ebd44118a4d4a6cb01f090a34c7b3
Rui Wen <ruiwen@dhcp-10-105-215-29.wireless.northwestern.private>
2023-01-16 18:54:39 -0600

    finished motors

--- simulate.py
+++ simulate.py
@@ -11,14 +11,25 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 
+backLeg_amplitude = numpy.pi/4
+backLeg_frequency = 10
+backLeg_phaseOffset = 0
+
+frontLeg_amplitude = numpy.pi/4
+frontLeg_frequency = 10
+frontLeg_phaseOffset = 0
+
 p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 frontLegSensorValues = numpy.zeros(1000)
 backLegSensorValues = numpy.zeros(1000)
 linAngles = numpy.linspace(0, 2*numpy.pi, 1000)
-targetAngles = numpy.sin(linAngles)
-numpy.save('data/targetAngles.npy', targetAngles)
+# targetAngles = numpy.pi/4*numpy.sin(linAngles)
+backLeg_targetAngles = backLeg_amplitude*numpy.sin(backLeg_frequency*linAngles+backLeg_phaseOffset)
+frontLeg_targetAngles = frontLeg_amplitude*numpy.sin(frontLeg_frequency*linAngles+frontLeg_phaseOffset)
+# numpy.save('data/backLeg_targetAngles.npy', backLeg_targetAngles)
+# numpy.save('data/frontLeg_targetAngles.npy', frontLeg_targetAngles)
 
 for i in range(1000):
     p.stepSimulation()
@@ -28,15 +39,18 @@ for i in range(1000):
     bodyIndex = robotId,
     jointName = "Torso_BackLeg",
     controlMode = p.POSITION_CONTROL,
-    targetPosition = random.uniform(-numpy.pi/4, numpy.pi/4),
+    #targetPosition = random.uniform(-numpy.pi/4, numpy.pi/4),
+    targetPosition = backLeg_targetAngles[i],
     maxForce = 60)
+
     pyrosim.Set_Motor_For_Joint(
     bodyIndex = robotId,
     jointName = "Torso_FrontLeg",
     controlMode = p.POSITION_CONTROL,
-    targetPosition = random.uniform(-numpy.pi/4, numpy.pi/4),
+    #targetPosition = random.uniform(-numpy.pi/4, numpy.pi/4),
+    targetPosition = frontLeg_targetAngles[i],
     maxForce = 60)
-    time.sleep(1/60)
+    time.sleep(1/240)
 
 # print(frontLegSensorValues)
 # print(backLegSensorValues)
